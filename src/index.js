e7d6b179f603cd06b891562edd0799a9c92c6880
78068360+RichardGabelman@users.noreply.github.com <78068360+actuallychaz@users.noreply.github.com>
2024-07-20 19:38:34 -0700

    Add basic index structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import './style.css';
 
-console.log("test");
+class navController {
+
+  constructor() {
+    this.home = document.querySelector("#home");
+    this.menu = document.querySelector("#menu");
+    this.about = document.querySelector("#about");
+    this.content = document.querySelector("#content");
+  }
+
+  initializeEventHandlers() {
+    this.home.addEventListener("click", () => {
+      console.log("home button pressed");
+    });
+
+    this.menu.addEventListener("click", () => {
+      console.log("menu button pressed");
+    });
+
+    this.about.addEventListener("click", () => {
+      console.log("about button pressed");
+    });
+  }
+
+  clearContent() {
+    this.content.textContent = '';
+  }
+}
+
+const controller = new navController();
+controller.initializeEventHandlers();
